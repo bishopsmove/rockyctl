a16b0fa8751f93be639bccd58bec54bb0b3989e2
Jim Ownby <jim.ownby@soltech.net>
2026-09-08 19:50:30 -0400

    Backing out fix-5

--- src/loop.ts
+++ src/loop.ts
@@ -103,7 +103,7 @@ async function runIterations(
       } catch (e) {
         ui.fail("TSC check failed!");
         store.update(task.id, { status: "pending" });
-        continue;
+        throw new Error("TSC check failed!");
       }
     }
 
